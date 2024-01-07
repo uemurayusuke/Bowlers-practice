ef90c520aa91022db2bcccafbdeb05f2d3d4e598
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-01-07 13:01:47 +0000

    ヘッダー設定-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  #ユーザー認証関係
   devise_scope :user do
   root to: 'public/sessions#new'
   end
@@ -13,4 +14,15 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+#会員関係
+namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+    get 'customers/unsubscribe'
+    get 'customers/withdraw'
+  end
+
+
 end
