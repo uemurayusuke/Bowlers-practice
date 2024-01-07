02c7bf384089ffa2817ed563efbcb6795239687b
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-01-07 05:34:15 +0000

    ユーザ認証実装[ログイン/新規登録設定中]

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,27 +1,11 @@
-# frozen_string_literal: true
-
 class Admins::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def after_sign_in_path_for(resource)
+    #会員一覧画面に遷移する。
+  end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def after_sign_out_path_for(resource)
+     #ログアウトしたら、ログイン画面に遷移する。
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,27 +1,24 @@
-# frozen_string_literal: true
-
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+def after_sign_in_path_for(resource)
+  #ログインしたら、マイページに遷移。
+end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+def after_sign_out_path_for(resource)
+  #ログアウトしたら、ログイン画面に遷移。
+end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+# private
 
-  # protected
+# def customer_state
+#   customer = Customer.find_by(email: params[:customer][:email])
+#   return if customer.nil?
+#   if customer.is_active
+#     return unless customer.valid_password?(params[:customer][:password])
+#   else
+#     redirect_to  new_customer_registration_path
+#   end
+# end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+
 end
