b0370bc9d0198167fb7dbdecdb24fc20962a3227
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-01-08 11:31:00 +0000

    投稿一覧機能[Start]

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 has_many :posts
+has_one_attached :profile_image
   # has_many :post_comments, dependent: :destroy
 
 end
