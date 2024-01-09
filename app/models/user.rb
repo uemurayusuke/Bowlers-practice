f8cb076b1cb70602c95858b968d1c0fcbd0ef961
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-01-09 08:14:38 +0000

    フォロー機能実装[Start]

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,22 @@ class User < ApplicationRecord
 has_many :posts
 has_many :favorites, dependent: :destroy
 has_many :post_comments, dependent: :destroy
-has_one_attached :profile_image
-  # has_many :post_comments, dependent: :destroy
 
+#フォロー機能に関する記述。理解不能
+has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+has_many :followers, through: :reverse_of_relationships, source: :follower
+
+has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+has_many :followings, through: :relationships, source: :followed
+
+
+
+
+
+
+
+
+has_one_attached :profile_image
 
 def get_profile_image(width, height)
   unless profile_image.attached?
@@ -16,5 +29,23 @@ def get_profile_image(width, height)
   end
   profile_image.variant(resize_to_limit: [width, height]).processed
 end
+
+
+
+ def follow(user)
+    relationships.create(followed_id: user.id)
+ end
+
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
+
+
+
 
 end
