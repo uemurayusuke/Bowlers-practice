930fe172ccc41a2538d9d779eb9e9115a4581761
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-01-09 00:42:08 +0000

    ユーザー機能実装[プロフィー編集完了]

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -8,10 +8,18 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
 
 
   def update
+     @user = current_user
+     if @user.update(user_params)
+      redirect_to user_path(@user)
+      #ユーザー詳細ページへ
+    else
+      render "edit"
+    end
   end
 
   def unsubscribe
@@ -19,4 +27,9 @@ class Public::UsersController < ApplicationController
 
   def withdraw
   end
+
+  def user_params
+    params.require(:user).permit(:user_name, :introduction, :profile_image)
+  end
+
 end
