0ba0e3679a82baab9160aad0dd7618f1cac046c9
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-01-09 13:05:47 +0000

    [エラー対応中]

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -19,6 +19,9 @@ class Public::PostsController < ApplicationController
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-has_many :posts
+has_many :posts, dependent: :destroy
 has_many :favorites, dependent: :destroy
 has_many :post_comments, dependent: :destroy
 
