984263b667dadf71c6557adca74a27628d59cf5e
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-01-11 12:43:38 +0000

    検索機能実装-2

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,4 +14,20 @@ end
 def get_post_image(width, height)
   post_image.variant(resize_to_limit: [width, height]).processed
 end
+
+#検索機能の記述
+ def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('title LIKE ?', content+'%')
+    elsif method == 'backward'
+      Book.where('title LIKE ?', '%'+content)
+    else
+      Book.where('title LIKE ?', '%'+content+'%')
+    end
+  end
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -46,8 +46,17 @@ end
     followings.include?(user)
   end
 
-
-
-
+#検索機能の記述
+def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
+  end
 
 end
