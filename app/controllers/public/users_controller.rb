e25ad7d6efbf4cd4f39a7fce60c825cf1c8cfb24
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-01-14 06:57:53 +0000

    レイアウト調整-4

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -36,10 +36,9 @@ class Public::UsersController < ApplicationController
   end
 
   def favorites
-    @user = User.find(params[:id])
-    favorites = Favorite.where(user_id: @user.id).pluck(:post_id)
+    user = User.find(params[:id])
+    favorites = Favorite.where(user_id: user.id).pluck(:post_id)
     @favorite_posts = Post.find(favorites)
-    @post = Post.find(params[:id])
   end
 
 
