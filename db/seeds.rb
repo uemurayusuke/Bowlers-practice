72ff20c13cd34199601415bb12904396125b1a88
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-01-14 10:02:43 +0000

    レイアウト調整-12[seedファイル設定完了]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,18 @@
+5.times do |n|
+  User.create!(
+    last_name: "試験#{n + 1}",
+    first_name: "太郎#{n + 1}",
+    last_name_kana: "テスト#{n + 1}",
+    first_name_kana: "タロウ#{n + 1}",
+    email: "test#{n + 1}@test.com",
+    user_name: "taro#{n + 1}",
+    introduction: "よろしく#{n + 1}",
+    password: "123456",
+    is_active: "true"
+  )
+end
+
+
 Admin.create!(
     email: "bowlers@gmail.com",
     password: "bowlers202311"
