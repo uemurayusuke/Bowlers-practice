7bd8cef64b18fbd5794dc4153f7975cace5e8adc
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-01-16 06:13:08 +0000

    ユーザーインターフェース[フラッシュメッセージ実装-2]

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,22 +2,20 @@ class Public::SessionsController < Devise::SessionsController
 before_action :user_state, only: [:create]
 
 def after_sign_in_path_for(resource)
+  flash[:notice] = "ログインしました！"
   user_path(current_user)
-  #ログインしたら、ユーザー詳細(ログインしているユーザー)に遷移
 end
 
 def after_sign_out_path_for(resource)
+  flash[:alert] = "ログアウトしました！"
   root_path
-  #ログアウトしたら、ログイン画面に遷移
 end
 
 
-#ゲストログインに関する記述
 def guest_sign_in
     user = User.guest
     sign_in user
     redirect_to user_path(user), notice: "guestuserでログインしました。"
-    #ゲストログインとしてマイページへ遷移する
 end
 
 private
@@ -29,7 +27,6 @@ def user_state
     return unless user.valid_password?(params[:user][:password])
   else
     redirect_to  new_user_registration_path
-    #新規登録画面へリダイレクト
   end
 end
 
