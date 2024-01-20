22733b3ce4640541374dae6289cebbd86e5689ed
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-01-20 08:15:14 +0000

    レイアウト[管理者側検索機能完了]

---
+++ app/controllers/admin/searches_controller.rb
@@ -0,0 +1,16 @@
+class Admin::SearchesController < ApplicationController
+
+  def search
+
+    @model = params[:model]
+		@content = params[:content]
+		@method = params[:method]
+		if @model == 'user'
+			@records = User.search_for(@content, @method)
+		elsif @model == 'post'
+			@records = Post.search_for(@content, @method)
+		elsif @model == 'tag'
+			@records = Tag.search_posts_for(@content, @method)
+		end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
 
 #管理者側
 namespace :admin do
+  get '/search', to: 'searches#search'
   resources :users, only:[:index, :show, :edit, :update]
   resources :posts, only:[:index, :show, :destroy]
   resources :post_comments, only:[:index, :destroy]
