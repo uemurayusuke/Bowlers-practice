8edcc94d2797ecf58e64c28c6e39e1f6075b4738
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-01-20 13:03:42 +0000

    レイアウト[管理側会員情報編集画面の修正完了]

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,6 +2,7 @@ class Admin::SessionsController < Devise::SessionsController
 
 
   def after_sign_in_path_for(resource)
+    flash.clear
     flash[:notice] = "ログインしました！"
     admin_users_path
   end
