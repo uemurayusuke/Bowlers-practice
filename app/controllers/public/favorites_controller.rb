86c5c83349699d3312a6a2520a1d0053820ace3d
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-01-23 01:53:16 +0000

    いいね非同期通信実装中

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,17 +3,17 @@ class Public::FavoritesController < ApplicationController
 
   def create
     post = Post.find(params[:post_id])
-    favorite = current_user.favorites.new(post_id: post.id)
-    favorite.save
-    redirect_to request.referer
+    @favorite = current_user.favorites.new(post_id: post.id)
+    @favorite.save
+    render template: 'replace_favorites_btn'
 
   end
 
   def destroy
     post = Post.find(params[:post_id])
-    favorite = current_user.favorites.find_by(post_id: post.id)
-    favorite.destroy
-    redirect_to request.referer
+    @favorite = current_user.favorites.find_by(post_id: post.id)
+    @favorite.destroy
+     render template: 'replace_favorites_btn'
 
   end
 
