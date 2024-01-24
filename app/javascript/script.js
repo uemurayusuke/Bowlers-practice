c621ff8c9db5ab87160a9091eb8b753645341449
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-01-24 06:06:08 +0000

    トップ画面にアニメーション実装完了

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,16 +1,22 @@
 // Swiperのオプションを定数化
 const opt = {
   loop: true,
+  effect: "fade", // フェードエフェクトを追加
   pagination: {
     el: '.swiper-pagination',
   },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  }
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  speed: 3000,
 }
 
 // Swiperを実行(初期化)
 $(document).on('turbolinks:load', function() {
-    let swiper = new Swiper('.swiper',opt);
+    let swiper = new Swiper('.swiper', opt);
 });
