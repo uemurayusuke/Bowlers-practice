fc30b8b85b4c29e3c510e8365f3a181fca48bf8b
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-01-25 06:08:26 +0000

    管理者側コメント検索実装中

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -3,17 +3,20 @@ class Admin::SearchesController < ApplicationController
 
 
 
-  def search
+def search
 
     @model = params[:model]
 		@content = params[:content]
 		@method = params[:method]
-		if @model == 'user'
+	if @model == 'user'
 			@records = User.search_for(@content, @method)
-		elsif @model == 'post'
+	elsif @model == 'post'
 			@records = Post.search_for(@content, @method)
-		elsif @model == 'tag'
+	elsif @model == 'tag'
 			@records = Tag.search_posts_for(@content, @method)
-		end
-  end
+	elsif @model == 'comment'
+		  @users = User.all
+			@records = PostComment.search_comments_for(@content, @method)
+	end
+end
 end
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -3,5 +3,16 @@ class PostComment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+def self.search_comments_for(content, method)
+
+    if method == 'perfect'
+      PostComment.where(comment: content)
+      #:nameはタグテーブルで保存されているカラム。
+    elsif method == 'partial'
+      PostComment.where('comment LIKE ?', '%'+content+'%')
+    end
 
 end
+
+end
+
