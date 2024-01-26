a23b0265ea0371b3ec0cda0b17dab0f72e87a806
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-01-26 14:15:42 +0000

    deploy setting-修正完了

--- db/migrate/20240108040215_create_posts.rb
+++ db/migrate/20240108040215_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
 
-      t.text :caption, default: "", null: false
+      t.text :caption, null: false
       t.integer :user_id
       t.timestamps
     end
