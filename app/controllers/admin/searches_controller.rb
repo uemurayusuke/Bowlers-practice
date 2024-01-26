65bdf3444390765303524bec145d1ad9e8cadef9
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-01-26 07:43:46 +0000

    テスト修正

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -9,14 +9,14 @@ def search
 		@content = params[:content]
 		@method = params[:method]
 	if @model == 'user'
-			@records = User.search_for(@content, @method)
+			@records = User.search_for(@content, @method).page(params[:page]).per(10)
 	elsif @model == 'post'
-			@records = Post.search_for(@content, @method)
+			@records = Post.search_for(@content, @method).page(params[:page]).per(5)
 	elsif @model == 'tag'
-			@records = Tag.search_posts_for(@content, @method)
+			@records = Tag.search_posts_for(@content, @method).page(params[:page]).per(5)
 	elsif @model == 'comment'
 		  @users = User.all
-			@records = PostComment.search_comments_for(@content, @method)
+			@records = PostComment.search_comments_for(@content, @method).page(params[:page]).per(10)
 	end
 end
 end
--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -7,11 +7,11 @@ class Public::SearchesController < ApplicationController
 		@content = params[:content]
 		@method = params[:method]
 		if @model == 'user'
-			@records = User.search_for(@content, @method)
+			@records = User.search_for(@content, @method).page(params[:page]).per(10)
 		elsif @model == 'post'
-			@records = Post.search_for(@content, @method)
+			@records = Post.search_for(@content, @method).page(params[:page]).per(5)
 		elsif @model == 'tag'
-			@records = Tag.search_posts_for(@content, @method)
+			@records = Tag.search_posts_for(@content, @method).page(params[:page]).per(5)
 		end
   end
 end
