8657e5e5c38f4ac0516e0921ad316e006b766ac7
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-02-15 07:57:29 +0000

    Rspec実装中

--- spec/system/03_finishing_touches_spec.rb
+++ spec/system/03_finishing_touches_spec.rb
@@ -1,38 +1,27 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe '[STEP3] 仕上げのテスト' do
-#   let(:user) { create(:user) }
-#   let!(:other_user) { create(:user) }
-#   let!(:book) { create(:book, user: user) }
-#   let!(:other_book) { create(:book, user: other_user) }
+describe '[STEP3] 仕上げのテスト' do
+  let(:user) { create(:user) }
 
-#   describe 'ログインしていない場合のアクセス制限のテスト: アクセスできず、ログイン画面に遷移する' do
-#     subject { current_path }
+  describe 'ログインしていない場合のアクセス制限のテスト: アクセスできず、ログイン画面に遷移する' do
+    subject { current_path }
 
-#     it 'ユーザ一覧画面' do
-#       visit users_path
-#       is_expected.to eq '/users/sign_in'
-#     end
-#     it 'ユーザ詳細画面' do
-#       visit user_path(user)
-#       is_expected.to eq '/users/sign_in'
-#     end
-#     it 'ユーザ情報編集画面' do
-#       visit edit_user_path(user)
-#       is_expected.to eq '/users/sign_in'
-#     end
-#     it '投稿一覧画面' do
-#       visit books_path
-#       is_expected.to eq '/users/sign_in'
-#     end
-#     it '投稿詳細画面' do
-#       visit book_path(book)
-#       is_expected.to eq '/users/sign_in'
-#     end
-#     it '投稿編集画面' do
-#       visit edit_book_path(book)
-#       is_expected.to eq '/users/sign_in'
-#     end
-#   end
+    it 'ユーザ一覧画面' do
+      visit users_path
+      is_expected.to eq '/users/sign_in'
+    end
+    it 'ユーザ詳細画面' do
+      visit user_path(user)
+      is_expected.to eq '/users/sign_in'
+    end
+    it 'ユーザ情報編集画面' do
+      visit edit_user_path(user)
+      is_expected.to eq '/users/sign_in'
+    end
+    it '投稿一覧画面' do
+      visit posts_path
+      is_expected.to eq '/users/sign_in'
+    end
+  end
 
-# end
+end
